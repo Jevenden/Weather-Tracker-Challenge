b625bc0c028c8407e7b791d9aca0d6ef7274c87b
John Evenden <johnthefightguy@gmail.com>
2022-06-06 18:00:19 -0400

    Minor stylistic changes, made the search button active, and began the code for the API call

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,3 @@
-// API Key from OpenWeather
-let APIKey = "b25eb13e2b9805c00e7a51eb01cd4a27";
-
-// Variable for storing the city that the user searched for
-let city = "";
-
 // Variables for targeting elements in HTML
 let searchField = $("#searchField");
 let submitBTN = $("#submitBTN");
@@ -13,14 +7,31 @@ let wind = $("#wind");
 let humidity = $("#humidity");
 let uvIndex = $("#uvIndex");
 
+// API Key from OpenWeather
+let APIKey = "b25eb13e2b9805c00e7a51eb01cd4a27";
+
+// Variable for storing the city that the user searched for
+let city = "";
+
+// Activates the API call when the user clicks the search button
+$("#submitBTN").on("click", getWeather);
+
+function getWeather() {
+  debugger;
+  city = searchField.val();
+  let queryURL =
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    APIKey;
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then();
+}
+
 // var queryURL =
 //   "http://api.openweathermap.org/data/2.5/weather?q=" +
 //   city +
 //   "&appid=" +
-//   APIKey;
-
-// $("#submit").click(function () {
-//   preventDefault();
-//   fetch(queryURL);
-//   $(".infoBox").val(fetch(queryURL));
-// });
+//   APIKey
