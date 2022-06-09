9c096770316653919fb1cf85c3940ce602e82e2e
John Evenden <johnthefightguy@gmail.com>
2022-06-09 17:49:39 -0400

    Added the 5 day forecast, complete with pertinent data, icons, and color coding for the UV Index, as awell as a key for better user understanding

--- assets/script.js
+++ assets/script.js
@@ -13,7 +13,7 @@ let uvBlock = $(".uvBlock");
 let weatherIcon = $(".weatherIcon");
 let currentDate = $("#currentDate");
 let weatherDesc = $(".weatherDesc");
-let date = moment().format("MMMM Do YYYY");
+let date = moment().format("dddd") + ", " + moment().format("MMMM Do");
 
 // API Key from OpenWeather
 let APIKey = "b25eb13e2b9805c00e7a51eb01cd4a27";
@@ -24,6 +24,7 @@ let city = "";
 // Activates the API call when the user clicks the search button
 $("#submitBTN").on("click", getWeather);
 
+// Function to make it initial API call and call the display functions
 function getWeather(event) {
   event.preventDefault();
   city = searchField.val();
@@ -42,6 +43,7 @@ function getWeather(event) {
   });
 }
 
+// Function to display the current-day weather info from the API call
 function displayWeather(response) {
   currentCity.html(response.name);
   currentDate.html(date);
@@ -51,6 +53,7 @@ function displayWeather(response) {
   humBlock.html("Humidity: " + response.main.humidity + "%");
 }
 
+// Function to make a second API call to retrieve information on the UV Index and a 5-day forecast
 function getUVI(response) {
   let lat = response.coord.lat;
   let lon = response.coord.lon;
@@ -59,7 +62,7 @@ function getUVI(response) {
     lat +
     "&lon=" +
     lon +
-    "&exclude=hourly,daily&appid=" +
+    "&units=imperial&appid=" +
     APIKey;
   $.ajax({
     url: queryURL,
@@ -72,19 +75,58 @@ function getUVI(response) {
     weatherIcon.html("<img src=" + iconURL + ">");
     weatherDesc.html(description);
     console.log(data);
-    displayUVI(data);
+    forecast(data);
   });
 }
 
-function displayUVI(data) {
-  if (data.current.uvi < 2) {
-    uvIndex.css("background-color", "lightblue");
-  } else if (data.current.uvi > 7) {
-    uvIndex.css("background-color", "tomato");
-  } else {
-    uvIndex.css("background-color", "lightyellow");
+function forecast(data) {
+  for (i = 0; i < 5; i++) {
+    let iconCode = data.daily[i].weather[0].icon;
+    let iconURL = "https://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+    let foreTemp = data.daily[i].temp.day;
+    let foreWind = data.daily[i].wind_speed;
+    let foreHum = data.daily[i].humidity;
+    let foreUV = data.daily[i].uvi;
+
+    $("#foreDate" + i).html(
+      moment()
+        .add(i + 1, "days")
+        .format("dddd") +
+        ", " +
+        moment()
+          .add(i + 1, "days")
+          .format("MMMM Do")
+    );
+    $("#foreIcon" + i).html("<img src=" + iconURL + ">");
+    $("#foreTemp" + i).html("Temp: " + foreTemp);
+    $("#foreWind" + i).html("Wind: " + foreWind);
+    $("#foreHum" + i).html("Hum: " + foreHum + "%");
+    $("#foreUV" + i).html("UV: " + foreUV);
+    $(".forecast").css("border", "black solid 1px");
+    $("#uvIndex").css("border", "black solid 1px");
+    $(".uvKeyLight").html("Low UV Index").css("background-color", "lightblue");
+    $(".uvKeyMed").html("Med UV Index").css("background-color", "lightyellow");
+    $(".uvKeyHot").html("High UV Index").css("background-color", "tomato");
+
+    if (data.daily[i].uvi <= 2) {
+      $("#fore" + i).css("background-color", "lightblue");
+    } else if (data.daily[i].uvi > 7) {
+      $("#fore" + i).css("background-color", "tomato");
+    } else {
+      $("#fore" + i).css("background-color", "lightyellow");
+    }
+
+    if (data.current.uvi <= 2) {
+      uvIndex.css("background-color", "lightblue");
+    } else if (data.current.uvi > 7) {
+      uvIndex.css("background-color", "tomato");
+    } else {
+      uvIndex.css("background-color", "lightyellow");
+    }
   }
 }
+
+// Function to evaluate and change the background color of the current forcast box accordingly
 
 // .catch(() => {
 //   currentCity.html(" Please enter a valid city");
